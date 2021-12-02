eccc293213989f00c5fef6bb7c4f9881805e3519
Samuel Erdö <e12022482@student.tuwien.ac.at>
2021-12-02 17:36:40 +0100

    day01 - part two

--- src/days/day01/solve.test.ts
+++ src/days/day01/solve.test.ts
@@ -1,4 +1,4 @@
-import { solvePartOne } from "./solve";
+import { solvePartOne, solvePartTwo } from "./solve";
 
 describe("Part one", () => {
     test("solves the example", () => {
@@ -18,5 +18,26 @@ describe("Part one", () => {
         const result = solvePartOne(input);
 
         expect(result).toEqual(7);
+    });
+});
+
+describe("Part two", () => {
+    test("solves the example", () => {
+        const input = `\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+`;
+
+        const result = solvePartTwo(input);
+
+        expect(result).toEqual(5);
     });
 });
--- src/days/day01/solve.ts
+++ src/days/day01/solve.ts
@@ -1,12 +1,15 @@
+import { count } from "./logic";
 import { parsePartOne } from "./parse";
 
 export const solvePartOne = (input: string) => {
-    let parsedInput = parsePartOne(input);
-    let counter = 0;
-    for (var i = 0; i < parsedInput.length - 1; i++) {
-        if (parsedInput[i] < parsedInput[i + 1]) counter++;
-    }
-    return counter;
+    return count(parsePartOne(input));
 };
 
-export const solvePartTwo = (input: string) => { };
+export const solvePartTwo = (input: string) => {
+    let parsedInput = parsePartOne(input);
+    let groupedArray = [];
+    for (var i = 0; i < parsedInput.length - 2; i++) {
+        groupedArray.push(parsedInput[i] + parsedInput[i + 1] + parsedInput[i + 2]);
+    }
+    return count(groupedArray);
+};
