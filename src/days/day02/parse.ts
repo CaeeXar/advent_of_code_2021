1753e3cc4b6bcd6eb57b9c9fdb4cde640bf54b59
Samuel Erdö <e12022482@student.tuwien.ac.at>
2021-12-02 23:30:13 +0100

    day02 - part two

--- src/days/day02/parse.ts
+++ src/days/day02/parse.ts
@@ -1,4 +1,3 @@
 export const parsePartOne = (input: string) => {
-    const test = input.split("\n");
-    return test;
+    return input.split("\n");
 };
--- src/days/day02/solve.ts
+++ src/days/day02/solve.ts
@@ -1,27 +1,27 @@
-import { parsePartOne } from "./parse";
+import { getData } from "./logic";
 
 export const solvePartOne = (input: string) => {
-    const parsedInput = parsePartOne(input);
-    let x = 0, y = 0, axis: string[] = [], distance: number[] = [];
-    parsedInput.forEach(element => {
-        let data = element.split(" ");
-        axis.push(data[0]);
-        distance.push(+data[1]);
-    });
+    let x = 0, y = 0, { axis, distance } = getData(input);
 
-    for (let i = 0; i < axis.length; i++) {
-        if (axis[i] == "forward") {
-            x += distance[i];
-        } else {
-            if (axis[i] == "down") {
-                y += distance[i];
-            } else {
-                y -= distance[i];
-            }
-        }
-    }
+    for (let i = 0; i < axis.length; i++)
+        axis[i] == "forward" ?
+            x += distance[i] :
+            y += (axis[i] == "up" ? -1 : 1) * distance[i];
 
     return x * y;
 };
 
-export const solvePartTwo = (input: string) => { };
+export const solvePartTwo = (input: string) => {
+    let x = 0, aim = 0, depth = 0, { axis, distance } = getData(input);
+
+    for (let i = 0; i < axis.length; i++) {
+        let v = distance[i];
+        if (axis[i] == "forward") {
+            x += v;
+            depth += aim * v;
+        }
+        else aim = aim + (axis[i] == "up" ? -1 : 1) * v;
+    }
+
+    return x * depth;
+};
