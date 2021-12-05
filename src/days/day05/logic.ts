82e394fc5fcbce93562b2de5ef614fcba6e4475d
Samuel Erdö <e12022482@student.tuwien.ac.at>
2021-12-05 23:22:28 +0100

    day05 - part two

--- src/days/day05/logic.ts
+++ src/days/day05/logic.ts
@@ -24,20 +24,58 @@ export const generateField = (size: number) => {
     return f;
 };
 
-export const fillField = (data: { x1: number, y1: number, x2: number, y2: number }[], field: number[][]) => {
-    let copy = _.cloneDeep(field);
+export const fillField = (data: { x1: number, y1: number, x2: number, y2: number }[], field: number[][], diagonal: boolean) => {
+    let copyField = _.cloneDeep(field), copyData = _.cloneDeep(data);
+
+    // zeichnet nur horizontale und vertikale ein ==> x1 = y1 oder y1 = y2
+    data = data.filter(d => d.x1 == d.x2 || d.y1 == d.y2);
 
     for (let i = 0; i < data.length; i++) {
         let { x1, y1, x2, y2 } = data[i];
 
         for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
             for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
-                copy[y][x]++;
+                copyField[y][x]++;
             }
         }
     }
 
-    return copy;
+    // diagonale erfordern eine Sonderlogik die per Funktionsparameter angesteuert werden kann
+    if (diagonal) {
+        copyField = fillFieldDiagonal(copyData.filter(d => d.x1 != d.x2 && d.y1 != d.y2), copyField);
+    }
+
+    return copyField;
+};
+
+const fillFieldDiagonal = (data: { x1: number, y1: number, x2: number, y2: number }[], field: number[][]) => {
+    let copyField = _.cloneDeep(field);
+    // zeichnet nur diagonal ein ==> x1 != x2 und y1 != y2
+
+    for (let i = 0; i < data.length; i++) {
+        let { x1, y1, x2, y2 } = data[i];
+
+        if (x1 > x2 && y1 > y2) {
+            for (let y = y1, x = x1; y >= y2 && x >= x2; y--, x--) {
+                copyField[y][x]++;
+            }
+        }
+        else if (x1 > x2 && y2 > y1) {
+            for (let y = y1, x = x1; y <= y2 && x >= x2; y++, x--) {
+                copyField[y][x]++;
+            }
+        } else if (x2 > x1 && y1 > y2) {
+            for (let y = y1, x = x1; y >= y2 && x <= x2; y--, x++) {
+                copyField[y][x]++;
+            }
+        } else if (x2 > x1 && y2 > y1) {
+            for (let y = y1, x = x1; y <= y2 && x <= x2; y++, x++) {
+                copyField[y][x]++;
+            }
+        }
+    }
+
+    return copyField;
 };
 
 export const countField = (field: number[][]) => {
